7e71933273efe8e71a0131998cf950af42eba401
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2022-07-10 18:55:46 +0530

    Component functions all re-evaluate but DOM updates selectively

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,25 @@
+import { useState } from "react";
+
+let firstTime = true;
+
 function App() {
-  return "Hello, world";
+  const [showParagraph, setShowParagraph] = useState(false);
+
+  const toggleParagraphHandler = () =>
+    setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+
+  if (firstTime) {
+    console.log("First run");
+    firstTime = false;
+  } else console.log("Run on update");
+
+  return (
+    <div>
+      <h1>Hi there!</h1>
+      {showParagraph && <p>This is new!</p>}
+      <button onClick={toggleParagraphHandler}>Toggle</button>
+    </div>
+  );
 }
 
 export default App;
